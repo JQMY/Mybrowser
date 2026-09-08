3d668c4a16a6c6302b9c2e9d86a3091ff00c1621
JQMY <jamiesscott2005@gmail.com>
2026-09-08 08:26:01 +0100

    Create app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.mybrowser"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.mybrowser"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
