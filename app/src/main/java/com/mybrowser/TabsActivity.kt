3bc3ae0536dcdea7e4e1358679f5058b8e815151
JQMY <jamiesscott2005@gmail.com>
2026-09-09 17:00:19 +0100

    Update TabsActivity.kt

--- app/src/main/java/com/mybrowser/TabsActivity.kt
+++ app/src/main/java/com/mybrowser/TabsActivity.kt
@@ -3,18 +3,62 @@ package com.mybrowser
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class TabsActivity : Activity() {
+
+    private lateinit var tabContainer: LinearLayout
+
+    private val tabs = mutableListOf(
+        BrowserTab("Google", "https://www.google.com")
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.tabs_page)
 
+        tabContainer = findViewById(R.id.tabContainer)
+
         val newTabButton: Button = findViewById(R.id.newTabButton)
 
         newTabButton.setOnClickListener {
-            finish()
+            createNewTab()
+        }
+
+        displayTabs()
+    }
+
+    private fun createNewTab() {
+
+        val newTabNumber = tabs.size + 1
+
+        tabs.add(
+            BrowserTab(
+                "New Tab $newTabNumber",
+                "https://www.google.com"
+            )
+        )
+
+        displayTabs()
+    }
+
+    private fun displayTabs() {
+
+        tabContainer.removeAllViews()
+
+        for ((index, tab) in tabs.withIndex()) {
+
+            val tabView = TextView(this)
+
+            tabView.text =
+                "${index + 1}. ${tab.title}\n${tab.url}"
+
+            tabView.textSize = 18f
+            tabView.setPadding(20, 25, 20, 25)
+
+            tabContainer.addView(tabView)
         }
     }
 }
